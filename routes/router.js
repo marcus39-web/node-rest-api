cd6fb57bc08c15c7c730ecdbb954ec964f2a46e2
jaderbass <joerg@jaderbass.de>
2026-02-05 09:07:46 +0100

    feat: add routes/router.js

---
+++ routes/router.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import express from 'express';
+
+const router = (app) => {
+  const options = {
+    root: 'public',
+  }
+
+  app.use(express.static('public'));
+
+  app.get('/', (req, res) => {
+    res.send('Trallalla');
+  });
+}
+
+export default router;
